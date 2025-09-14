46a9ea34bb659377f485e1fb1dca4ca825d6d7e7
gin <2440016523@qq.com>
2025-09-14 18:49:03 +0800

    NFTmarket contract

--- NFTmarket/NFTmarket-contract/hardhat.config.js
+++ NFTmarket/NFTmarket-contract/hardhat.config.js
@@ -9,19 +9,9 @@ const { config: dotenvConfig } = require("dotenv")
 const { resolve } = require("path")
 dotenvConfig({ path: resolve(__dirname, "./.env") })
 
+const SEPOLIA_ALCHEMY_AK = process.env.SEPOLIA_ALCHEMY_AK
 const SEPOLIA_PK_ONE = process.env.SEPOLIA_PK_ONE
 const SEPOLIA_PK_TWO = process.env.SEPOLIA_PK_TWO
-if (!SEPOLIA_PK_ONE) {
-  throw new Error("Please set at least one private key in a .env file")
-}
-
-const MAINNET_PK = process.env.MAINNET_PK
-const MAINNET_ALCHEMY_AK = process.env.MAINNET_ALCHEMY_AK
-
-const SEPOLIA_ALCHEMY_AK = process.env.SEPOLIA_ALCHEMY_AK
-if (!SEPOLIA_ALCHEMY_AK) {
-  throw new Error("Please set your SEPOLIA_ALCHEMY_AK in a .env file")
-}
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -39,20 +29,10 @@ module.exports = {
     }
   },
   networks: {
-    mainnet: {
-      url: `https://eth-mainnet.g.alchemy.com/v2/${MAINNET_ALCHEMY_AK}`,
-      accounts: [`${MAINNET_PK}`],
-      saveDeployments: true,
-      chainId: 1,
-    },
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${SEPOLIA_ALCHEMY_AK}`,
       accounts: [`${SEPOLIA_PK_ONE}`, `${SEPOLIA_PK_TWO}`],
     },
-    // optimism: {
-    //   url: `https://rpc.ankr.com/optimism`,
-    //   accounts: [`${MAINNET_PK}`],
-    // },
   },
   gasReporter: {
     currency: "USD",
